d68809d32e8e64309e2c3cdd19ce1f8b4bdc8391
srinivasan0328 <srinivasanakash0328@gmail.com>
2025-04-01 20:51:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+    let taskText = taskInput.value.trim();
+    
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+    
+    let li = document.createElement("li");
+    li.innerHTML = `${taskText} <button class='delete' onclick='deleteTask(this)'> X </button>`;
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
